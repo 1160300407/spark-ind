5ac7f8939888f18588c29462464fe64a85e6b366
1160300407 <290690260@qq.com>
2020-11-27 14:23:59 +0800

    combine

--- bbdis/src/com/hadoop/spark/bbdis.scala
+++ bbdis/src/com/hadoop/spark/bbdis.scala
@@ -4,19 +4,16 @@ import org.apache.spark.SparkContext
 import scala.Array.concat
 
 object bbdis {
-  def FILE_NAME: String = "/user/litianfeng/input/scop.txt"
   def APP_NAME: String = "b&b"
   def typeList: Array[String] = Array("string", "int")
-  def SAVE_PATH1: String = "/user/litianfeng/output-bb1"
-
+  def MASTER_NAME: String = "local[*]"
   //line中所有string全为数字，则返回true
   def isTypeInt(line: Seq[String]): Boolean = {
     val regex = """^\d+$""".r
     return line.forall(s => regex.findFirstMatchIn(s) != None)
   }
-
   //计算line <= base[?], 返回一个数组，如[1,2,3],表示第一列包含依赖于第二列和第三列。
-  def calculateInd(line: (Seq[String], Int), base: Array[(Seq[String], Int)]) = {
+  def calculateInd(line: (Array[String], String), base: Array[(Array[String], String)]) = {
     val lhs = Array(line._2)
     val rhss = base.filter(x => {
       val rhs = x._1.toSet
@@ -27,24 +24,23 @@ object bbdis {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName(APP_NAME)
+    if (args(0) == "local")
+      conf.setMaster(MASTER_NAME)
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile(args(0))
 
-    //分隔开
-    val seperateTF = textFile.map(line => line.split(" ")).collect().toSeq
-    //将行转置为列， 并在新的行前面加入该行的最小值和最大值，加速后续的包含判断
-    val columnsData = sc.parallelize({
-      val temp = seperateTF.transpose
-      temp.foreach(x =>
-        x.min +: x.max +: x
-      )
-      temp.zipWithIndex
+    val textFile = sc.wholeTextFiles(args(1))
+    val columnsData = textFile.flatMap(file => {
+      val filename = file._1.substring(file._1.lastIndexOf('/')+1, file._1.lastIndexOf('.'))
+      //将行转置为列， 并在新的行前面加入该行的最小值和最大值，加速后续的包含判断
+      val sepe = file._2.split("\n").map(line => line.split(" ")).transpose
+      sepe.foreach(x => x.min +: x.max +: x)
+      sepe.zipWithIndex.map(pair => (pair._1 -> (filename+"-"+pair._2.toString)))
     })
 
     //用typeIntData存储所有数字属性数据 NoInt存储字符
     val typeIntData = columnsData.filter(line => isTypeInt(line._1) == true)
     val typeNoIntData = columnsData.filter(line => isTypeInt(line._1) == false)
-    //  .map(line=>(line._1.sorted,line._2)) //sort!
+
     //将两类数据全部发送到所有节点
     val baseIntData = sc.broadcast(typeIntData.collect)
     val baseNoIntData = sc.broadcast(typeNoIntData.collect)
@@ -52,10 +48,10 @@ object bbdis {
     //两类数据分别计算ind
     val ind = typeNoIntData.map(line =>
       calculateInd(line, baseNoIntData.value)
-    ).cache()
+    )
     val ind2 = typeIntData.map(line =>
       calculateInd(line, baseIntData.value)
-    ).cache()
+    )
 
     println("ind calc over!")
     ind.collect()
@@ -65,8 +61,9 @@ object bbdis {
       .filter(x => x.length > 1)
       .foreach(x=>{println(x(0)+":"+x.drop(1).mkString(","))})
 
-    ind.saveAsTextFile(args(1)+"-type1")
-    ind2.saveAsTextFile(args(1)+"-type2")
-    //ind2.saveAsTextFile(SAVE_PATH2)
+    if (args.size > 2) {
+      ind.saveAsTextFile(args(1) + "-type1")
+      ind2.saveAsTextFile(args(1) + "-type2")
+    }
   }
 }
--- sindydis/src/com/hadoop/spark/sindydis.scala
+++ sindydis/src/com/hadoop/spark/sindydis.scala
@@ -4,10 +4,13 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object sindydis {
   def APP_NAME: String = "sindy"
+  def MASTER_NAME: String = "local[*]"
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName(APP_NAME)
+    if (args(0) == "local")//本地模式
+        conf.setMaster(MASTER_NAME)
     val sc = new SparkContext(conf)
-    val textFile = sc.wholeTextFiles(args(0))
+    val textFile = sc.wholeTextFiles(args(1))
     val value2attr = textFile.flatMap(file => {
       val filename = file._1.substring(file._1.lastIndexOf('/')+1, file._1.lastIndexOf('.'))
       file._2.split("\n").flatMap(line => {
@@ -18,7 +21,7 @@ object sindydis {
     //聚合所有相同value的索引项，生成一个set
     val value2attrs = value2attr.reduceByKey((a, b) => {
       a ++ b
-    }).filter(a => a._2.size > 1)
+    })
 
     //对每个value对应的set，生成candidates， i -> (All - i)
     val candidate = value2attrs.flatMap(a => {
@@ -30,8 +33,9 @@ object sindydis {
     })
 
     //对相同index的candidate进行聚合，用集合交
-    val ind = candidate.reduceByKey((a, b) => a.intersect(b)).cache()
+    val ind = candidate.reduceByKey((a, b) => a.intersect(b)).filter(x => x._2.size != 0).cache()
     ind.collect().foreach(println)
-    ind.saveAsTextFile(args(1))
+    if (args.size > 2)
+      ind.saveAsTextFile(args(2))
   }
 }
