6734b5ae97df4bd687982dfc57ed2447ef2557de
1160300407 <290690260@qq.com>
2020-12-01 16:00:14 +0800

    backup

--- sindydis/src/com/hadoop/spark/sindydis.scala
+++ sindydis/src/com/hadoop/spark/sindydis.scala
@@ -10,13 +10,14 @@ object sindydis {
     if (args(0) == "local")//本地模式
         conf.setMaster(MASTER_NAME)
     val sc = new SparkContext(conf)
-    val textFile = sc.wholeTextFiles(args(1))
+   /* val textFile = sc.wholeTextFiles(args(1))
     val value2attr = textFile.flatMap(file => {
       val filename = file._1.substring(file._1.lastIndexOf('/')+1, file._1.lastIndexOf('.'))
       file._2.split("\n").flatMap(line => {
         line.split(" ").zipWithIndex.map(pair => (pair._1 -> Set(filename+"-"+pair._2.toString)))
       })
-    })
+    })*/
+
 
     //聚合所有相同value的索引项，生成一个set
     val value2attrs = value2attr.reduceByKey((a, b) => {
