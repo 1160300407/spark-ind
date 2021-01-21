eeeeded7f4d5bee8135ddf0f108bace4cd7237bc
1160300407 <290690260@qq.com>
2021-01-21 20:59:21 +0800

    deleted

--- bbdis/src/com/hadoop/spark/bbdis.scala
+++ bbdis/src/com/hadoop/spark/bbdis.scala
@@ -1,14 +1,15 @@
 package com.hadoop.spark
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapreduce.lib.input.{FileSplit, TextInputFormat}
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.NewHadoopRDD
+
 import scala.Array.concat
 
 object bbdis {
-  def FILE_NAME: String = "/user/litianfeng/input/scop.txt"
   def APP_NAME: String = "b&b"
   def typeList: Array[String] = Array("string", "int")
-
-  def SAVE_PATH1: String = "/user/litianfeng/output-bb1"
 
   //line中所有string全为数字，则返回true
   def isTypeInt(line: Seq[String]): Boolean = {
@@ -17,57 +18,79 @@ object bbdis {
   }
 
   //计算line <= base[?], 返回一个数组，如[1,2,3],表示第一列包含依赖于第二列和第三列。
-  def calculateInd(line: (Seq[String], Int), base: Array[(Seq[String], Int)]) = {
-    val lhs = Array(line._2)
+  def calculateInd(line: (String, List[String]), base: Array[(String, List[String])]) = {
     val rhss = base.filter(x => {
-      val rhs = x._1.toSet
-      (line._2 != x._2).&&(line._1.forall(s => rhs.contains(s)))
-    }).map(x => x._2)
-    concat(lhs, rhss)
+      val rhs = x._2.toSet
+      (line._1 != x._1).&&(line._2.forall(s => rhs.contains(s)))
+    }).map(x => x._1)
+    rhss
   }
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName(APP_NAME)
+    if (args(0) == "yes") //本地模式
+        conf.setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile(args(0))
+    val fc = classOf[TextInputFormat]
+    val kc = classOf[LongWritable]
+    val vc = classOf[Text]
 
-    //分隔开
-    val seperateTF = textFile.map(line => line.split(" ")).collect().toSeq
-    //将行转置为列， 并在新的行前面加入该行的最小值和最大值，加速后续的包含判断
-    val columnsData = sc.parallelize({
-      val temp = seperateTF.transpose
-      temp.foreach(x =>
-        x.min +: x.max +: x
-      )
-      temp.zipWithIndex
-    })
+    //val path :String = "file:///Users/litianfeng/PycharmProjects/lrhs"
+    //val path : String = "file:///Users/litianfeng/IdeaProjects/SparkFirst/table"
+    val path : String = args(1)
+    val text = sc.newAPIHadoopFile(path, fc ,kc, vc, sc.hadoopConfiguration)
+
+    val linesWithFileNames = text.asInstanceOf[NewHadoopRDD[LongWritable, Text]]
+      .mapPartitionsWithInputSplit((inputSplit, iterator) => {
+        val file = inputSplit.asInstanceOf[FileSplit]
+        iterator.map(tup => (file.getPath, tup._2))
+      })
+
+    val value2attr = linesWithFileNames.flatMap(p =>{
+      val filename = p._1.toString.substring(p._1.toString.lastIndexOf('/')+1)
+      val lines = p._2.toString.split("\n")
+      val result = lines.flatMap(l => {
+        l.split(" ").zipWithIndex.map(
+          pair => (pair._1 -> (filename+"-"+pair._2.toString))
+        )
+      })
+      result
+    }).cache()
+
+    val t = value2attr.groupBy(_._2)
+      .mapValues(_.map(_._1))
+      .collectAsMap()
+      .map(s => (s._1,s._2.toList))
+      .toSeq
+      .map(s => (s._1, s._2.min +: s._2.max +: s._2))//加入最小和最大值，加速后续的包含判断
+    val columnsData = sc.parallelize(t)//转置并生成RDD
 
     //用typeIntData存储所有数字属性数据 NoInt存储字符
-    val typeIntData = columnsData.filter(line => isTypeInt(line._1) == true)
-    val typeNoIntData = columnsData.filter(line => isTypeInt(line._1) == false)
-    //  .map(line=>(line._1.sorted,line._2)) //sort!
+    val typeIntData = columnsData.filter(line => isTypeInt(line._2) == true)
+    val typeNoIntData = columnsData.filter(line => isTypeInt(line._2) == false)
     //将两类数据全部发送到所有节点
     val baseIntData = sc.broadcast(typeIntData.collect)
     val baseNoIntData = sc.broadcast(typeNoIntData.collect)
 
     //两类数据分别计算ind
     val ind = typeNoIntData.map(line =>
-      calculateInd(line, baseNoIntData.value)
+      line._1->calculateInd(line, baseNoIntData.value)
     ).cache()
+
     val ind2 = typeIntData.map(line =>
-      calculateInd(line, baseIntData.value)
+      line._1->calculateInd(line, baseIntData.value)
     ).cache()
 
-    println("ind calc over!")
     ind.collect()
-      .filter(x => x.length > 1)
-      .foreach(x=> {println(x(0)+":"+x.drop(1).mkString(","))})
+      .filter(x => x._2.length != 0)
+      .foreach(x=>println(x._1+","+x._2.toSet.toString()))
     ind2.collect()
-      .filter(x => x.length > 1)
-      .foreach(x=>{println(x(0)+":"+x.drop(1).mkString(","))})
+      .filter(x => x._2.length != 0)
+      .foreach(x=>println(x._1+","+x._2.toSet.toString()))
 
-    ind.saveAsTextFile(args(1)+"-type1")
-    ind2.saveAsTextFile(args(1)+"-type2")
-    //ind2.saveAsTextFile(SAVE_PATH2)
+    if (args.size > 2) {
+      ind.saveAsTextFile(args(2)+"-String")
+      ind2.saveAsTextFile(args(2)+"-Int")
+    }
   }
 }
--- sindydis/src/com/hadoop/spark/sindydis.scala
+++ sindydis/src/com/hadoop/spark/sindydis.scala
@@ -1,45 +1,57 @@
 package com.hadoop.spark
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapreduce.lib.input.{FileSplit, TextInputFormat}
+import org.apache.spark.rdd.{NewHadoopRDD}
 
 object sindydis {
-  def FILE_NAME : String = "/user/litianfeng/input/adult.data,/user/litianfeng/input/adult.test"
-  //def FILE_NAME: String = "/user/litianfeng/input/scop.txt"
-  def SAVE_PATH: String = "/user/litianfeng/output-sindy"
   def APP_NAME: String = "sindy"
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName(APP_NAME)
+    if (args(0) == "yes")//本地模式
+      conf.setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile(args(0))
-    //对每个value不加区分的生成倒排索引
-    val value2attr = textFile.flatMap(line => {
-      val words = line.split(" ")
-      var amap: Map[String, Set[Int]] = Map()
-      for (i <- 0 until words.length)
-        amap += (words(i) -> Set(i))
-      amap
-    })
-    //TODO:可改为：
-    // val value2attr = textFile.flatMap(line => line.split(" ").zipWithIndex)
-    // + combineByKey函数
+
+    val fc = classOf[TextInputFormat]
+    val kc = classOf[LongWritable]
+    val vc = classOf[Text]
+
+    //val path :String = "file:///Users/litianfeng/PycharmProjects/lrhs"
+    //val path : String = "file:///Users/litianfeng/IdeaProjects/SparkFirst/table"
+    val path : String = args(1)
+    val text = sc.newAPIHadoopFile(path, fc ,kc, vc, sc.hadoopConfiguration)
+    val linesWithFileNames = text.asInstanceOf[NewHadoopRDD[LongWritable, Text]]
+      .mapPartitionsWithInputSplit((inputSplit, iterator) => {
+        val file = inputSplit.asInstanceOf[FileSplit]
+        iterator.map(tup => (file.getPath, tup._2))
+      })
+
+    //生成倒排索引
+    val value2attr = linesWithFileNames.flatMap(p =>{
+      val filename = p._1.toString.substring(p._1.toString.lastIndexOf('/')+1)
+      val lines = p._2.toString.split("\n")
+      val result = lines.flatMap(l => {
+          l.split(" ").zipWithIndex.map(
+            pair => (pair._1 -> Set(filename+" "+pair._2.toString))
+          )
+      })
+      result
+    }).cache()
 
     //聚合所有相同value的索引项，生成一个set
     val value2attrs = value2attr.reduceByKey((a, b) => {
       a ++ b
-    }).filter(a => a._2.size > 1)
-
-    //对每个value对应的set，生成candidates， i -> (All - i)
-    val candidate = value2attrs.flatMap(a => {
-      var amap: Map[Int, Set[Int]] = Map()
-      for (i <- a._2) {
-        amap += (i -> (a._2 - i))
-      }
-      amap
     })
 
+    //对每个value对应的set，生成candidates， i -> (All - i)
+    val candidate = value2attrs.flatMap(a => a._2.map(s=>s->(a._2-s)))
+
     //对相同index的candidate进行聚合，用集合交
-    val ind = candidate.reduceByKey((a, b) => a.intersect(b)).cache()
+    val ind = candidate.reduceByKey((a, b) => a.intersect(b)).filter(x=>x._2.nonEmpty).cache()
     ind.collect().foreach(println)
-    ind.saveAsTextFile(args(1))
+
+    if (args.size > 2) ind.saveAsTextFile(args(2))
   }
 }
